0ba4c78c363b63f64dc434cfbe2d911ca43f0ee0
Navenrakumar Karneshwar <navenrakumarkarneshwar@gmail.com>
2025-04-04 11:29:01 +0530

    Add files via upload

---
+++ website/billing.php
@@ -0,0 +1,104 @@
+<?php
+
+include 'config.php';
+
+if(isset($_REQUEST["submit"])){
+
+  $id = $_REQUEST["id"];
+  $name = $_REQUEST["name"];
+  $period = $_REQUEST["period"];
+  
+
+  $ins = "INSERT INTO billing (id,name,period) VALUES('$id','$name','$period')";
+  $query = mysqli_query($connection,$ins);
+}
+
+
+?>
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    
+  </head>
+<body>
+
+
+
+<!---Navbar start--->
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="admin-login.php"><img src="logo.jpg" alt="Logo"></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#"> Gym Management System </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="billing.php">Billing</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="coach.php">Coach</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="members.php">Members</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="receptionist.php">Receptionist</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+<!---Navbar end--->
+
+<!---form start--->
+<form>
+  <div class="form-row">
+    <div class="form-group col-md-6">
+      <label for="inputEmail4">Member ID</label>
+      <input type="text" name="id" class="form-control" id="inputEmail4" placeholder="ID">
+    </div>
+    <div class="form-group col-md-6">
+      <label for="inputPassword4">Member Name</label>
+      <input type="text" name="name" class="form-control" id="inputPassword4" placeholder="NAME">
+    </div>
+  </div>
+  <div class="form-group">
+    <label for="inputAddress">Period</label>
+    <input type="text" name="period" class="form-control" id="inputAddress" placeholder="Date">
+  </div>
+  <div class="form-group">
+    <input type="submit" name="submit" class="form-control" id="inputAddress" placeholder="submit">
+  </div>
+  
+  <button type="submit" class="btn btn-primary">Sign in</button>
+</form>
+
+<!---form end--->
+
+
+
+
+
+
+
+
+
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    
+</body>
+</html>
